3551a17222137c09d684f37fc556f4e24573b17a
hanZhiChen <chenhanzhi233@gmail.com>
2020-07-09 19:54:48 +0800

    解决了在RecycleView的item中填写数据会错乱的问题，但还存在item的布局会随着下拉操作而错乱。

--- app/src/main/java/com/example/adapters/VentilationTableAdapter.java
+++ app/src/main/java/com/example/adapters/VentilationTableAdapter.java
@@ -1,7 +1,11 @@
 package com.example.adapters;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,21 +27,45 @@ import java.util.List;
 
 public class VentilationTableAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-//    private static final String TAG = "VentilationTableAdapter";
-    private List<String> mVentilationField;
+    private static final String TAG = "VentilationTableAdapter";
+
+    private Context mContext;
+    private Object[] mVentilationFields;
     private final static int FIRST_VIEW_TYPE = 1;//第一种布局标识
     private final static int SECOND_VIEW_TYPE = 2;//第二种布局标识
     private final static int THIRD_VIEW_TYPE = 3;//第二种布局标识
-    private RecyclerView.ViewHolder holder;
 
 
-    public VentilationTableAdapter(List<String> fieldList){
-        mVentilationField = fieldList;
+    //显示条数
+    private final int mSimpleNum = 20;
+
+    //ViewHolder 创建次数
+    private int mCreateTimes = 0;
+
+    //ViewHolder 绑定次数
+    private int mBindTimes = 0;
+
+     //已经绑定文本变化监听器
+    private final boolean mBoundWatcher = true;
+
+     //输入框文本缓存
+    private SparseArray<String> mTextCache = new SparseArray<>();
+
+
+
+    public interface  SaveEditListener{
+        void saveEdit(String fieldName, String data);
+    }
+
+
+    public VentilationTableAdapter(Object[] fields, Context mContext){
+        this.mVentilationFields = fields;
+        this.mContext = mContext;
     }
 
     @Override
     public int getItemViewType(int position) {
-        String fieldStr = mVentilationField.get(position);
+        String fieldStr = (String)mVentilationFields[position];
         switch (fieldStr){
             case "roadway":
                 return SECOND_VIEW_TYPE;
@@ -61,17 +89,22 @@ public class VentilationTableAdapter extends RecyclerView.Adapter<RecyclerView.V
                     parent, false);
             EditHolder editHolder = new EditHolder(view);
 
+            view.setTag(editHolder);
+            Log.i("onCreateViewHolder", "CreateTimes == " + mCreateTimes++);
+
             return  editHolder;
 
         }else if(viewType == SECOND_VIEW_TYPE){
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.datafillingspinner_layout,
                     parent, false);
             SpinnerHolder spinnerHolder = new SpinnerHolder(view);
+            Log.i("onCreateViewHolder", "CreateTimes == " + mCreateTimes++);
             return  spinnerHolder;
         }else{
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.datefilling_layout,
                     parent, false);
             DateTimeSelectHolder dateTimeSelectHolder = new DateTimeSelectHolder(view);
+            Log.i("onCreateViewHolder", "CreateTimes == " + mCreateTimes++);
             return  dateTimeSelectHolder;
         }
 
@@ -79,9 +112,40 @@ public class VentilationTableAdapter extends RecyclerView.Adapter<RecyclerView.V
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        String fieldNameString = mVentilationField.get(position);
+        Log.i("onBindViewHolder", "BindTimes == " + mBindTimes++ + " Position == " + holder.getAdapterPosition());
+
+        Log.i("TEXT_SHOW", "ShowPosition == " + holder.getAdapterPosition());
+        String fieldNameString = (String)mVentilationFields[position];
         if(holder instanceof EditHolder){
-            ((EditHolder)holder).fieldName.setText(fieldNameString);
+            EditHolder editHolder = (EditHolder)holder;
+
+
+
+            editHolder.editText.setText(mTextCache.get(holder.getAdapterPosition()));
+
+            // 如果已经绑定文本变化监听器不再次绑定
+            if(editHolder.editText.getTag()!=null && editHolder.editText.getTag() instanceof TextWatcher){
+                return;
+            }
+//            holder.setIsRecyclable(false);
+
+
+
+
+
+            //添加editText的监听事件
+            TextWatcher textWatcher = new TextSwitcher(editHolder);
+            editHolder.editText.addTextChangedListener(textWatcher);
+            editHolder.editText.setTag(textWatcher);
+
+
+
+            editHolder.fieldName.setText(fieldNameString);
+
+
+
+
+
         }else if(holder instanceof SpinnerHolder){
             ((SpinnerHolder)holder).fieldName.setText(fieldNameString);
 
@@ -110,8 +174,9 @@ public class VentilationTableAdapter extends RecyclerView.Adapter<RecyclerView.V
 
     @Override
     public int getItemCount() {
-        return mVentilationField.size();
+        return mVentilationFields.length;
     }
+
     static class EditHolder extends RecyclerView.ViewHolder{
         TextView fieldName;
         EditText editText;
@@ -142,6 +207,43 @@ public class VentilationTableAdapter extends RecyclerView.Adapter<RecyclerView.V
             fieldName = (TextView) view.findViewById(R.id.field_text);
             dateEdit = (EditText) view.findViewById(R.id.date_edit);
             timeEdit = (EditText) view.findViewById(R.id.time_edit);
+        }
+    }
+
+    class TextSwitcher implements TextWatcher {
+
+        private EditHolder mHolder;
+
+        public TextSwitcher(EditHolder mHolder) {
+            this.mHolder = mHolder;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //用户输入完毕后，处理输入数据，回调给主界面处理
+            // 如果填入数据与缓存数据相同返回
+            if (TextUtils.equals(mTextCache.get(mHolder.getAdapterPosition()), s.toString())) {
+                return;
+            }
+
+            mTextCache.put(mHolder.getAdapterPosition(), s.toString());
+            Log.i("TEXT_PUT", "PutPosition == " + mHolder.getAdapterPosition());
+
+
+            SaveEditListener listener = (SaveEditListener) mContext;
+            if (s != null) {
+                listener.saveEdit(mHolder.fieldName.getText().toString(), s.toString());
+            }
         }
     }
 
--- app/src/main/java/com/example/uicustomviews/VentilationTableActivity.java
+++ app/src/main/java/com/example/uicustomviews/VentilationTableActivity.java
@@ -2,6 +2,10 @@ package com.example.uicustomviews;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,18 @@ import com.example.pojo.VentilationData;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class VentilationTableActivity extends BaseActivity {
+public class VentilationTableActivity extends BaseActivity implements VentilationTableAdapter.SaveEditListener{
     private static final String TAG = "VentilationTableActivit";
 
-    private List<String> ventilationDataFields = new ArrayList<>();
+//    private List<String> ventilationDataFields = new ArrayList<>();
+
+    private Map<String, String> ventilationDataMap = new HashMap<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +36,33 @@ public class VentilationTableActivity extends BaseActivity {
 
         super.hideDefaultTitle();
 
+        //更改title的文本，以便与本活动对应
+        TextView titleText = (TextView)findViewById(R.id.title_text);
+        titleText.setText("井下通风数据采集页");
+        titleText.setTextSize(20);
+
         initVentilationDataFields();//初始化通风列表字段数据
 
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        VentilationTableAdapter adapter = new VentilationTableAdapter(ventilationDataFields);
+
+        Set<String> set = ventilationDataMap.keySet();
+
+        VentilationTableAdapter adapter = new VentilationTableAdapter(set.toArray(), VentilationTableActivity.this);
         recyclerView.setAdapter(adapter);
 
-        findViewById(R.id.data_edit);
+        Button commitButton = (Button) findViewById(R.id.title_empty);
+        commitButton.setText("提交数据");
+        commitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSave(v);
+            }
+        });
+
+
 
 
     }
@@ -44,10 +71,33 @@ public class VentilationTableActivity extends BaseActivity {
         Class ventilationDataClass = VentilationData.class;
         Field[] fields = ventilationDataClass.getDeclaredFields();
         for(Field field : fields){
-            ventilationDataFields.add(field.getName());
+            ventilationDataMap.put(field.getName(), null);
         }
 //        Log.d(TAG, ventilationDataFields.toString());
     }
 
 
+    @Override
+    public void saveEdit(String fieldName, String data) {
+        ventilationDataMap.put(fieldName, data);
+    }
+
+    void onSave(View view){
+        //处理存储edittext的map
+        //如判断客户名称是否填写且不为空格
+        Set<String> keySet = ventilationDataMap.keySet();
+        for(String str : keySet){
+            Log.d(TAG, str + " : " + ventilationDataMap.get(str));
+        }
+//        if(map.get(0)!=null&&!map.get(0).trim().equals("")){
+//            //遍历处理map存储的内容
+//            for (int i=0; i<ventilationDataFields.size(); i++){
+//                Log.d(TAG, ventilationDataFields.get(i)+" : "+map.get(i));
+//
+//            }
+//
+//        }else{
+//            Toast.makeText(this,"客户名称必填且不可为空",Toast.LENGTH_SHORT).show();
+//        }
+    };
 }
